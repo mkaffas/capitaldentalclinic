016f5c5da0c1c1d2192f0942d60ad6efbcecb914
amr.hesham <amr.hesham@odootec.com>
2021-12-20 11:29:42 +0400

    edit

--- warehouse_location_access_app/models/stock_location_warehouse_limitation.py
+++ warehouse_location_access_app/models/stock_location_warehouse_limitation.py
@@ -19,6 +19,7 @@ class ResUsers(models.Model):
 
 class Orderpoint(models.Model):
     _inherit = 'stock.warehouse.orderpoint'
+
     # _inherit = 'mail.thread'
 
     def send_products(self):
@@ -31,10 +32,35 @@ class Orderpoint(models.Model):
             'stock.group_stock_manager').users]
 
         if partners:
-            self.env['product.product'].sudo().search([],limit=1).sudo().message_post(
+            self.env['product.product'].sudo().search([], limit=1).sudo().message_post(
                 partner_ids=partners,
                 subject="Products to Replenishment",
-                body= body,
+                body=body,
                 message_type='comment',
                 subtype_id=self.env.ref('mail.mt_note').id, )
 
+
+class Warehouse(models.TransientModel):
+    _name = 'warehouse.orderpoint.wizard'
+
+    src_location_id = fields.Many2one(comodel_name="stock.location", string="Location", required=True, )
+
+    def create_transfer(self):
+        stock_ids = self.env['stock.warehouse.orderpoint'].browse(self._context.get('active_ids', False))
+        list_products = []
+        location_id = False
+        for line in stock_ids:
+            vals = {}
+            vals['product_id'] = line.product_id.id
+            location_id = line.location_id
+            vals['product_uom_qty'] = line.qty_to_order
+            list_products.append((0, 0, vals))
+        obj = self.env['stock.picking']
+        obj_picking = self.env['stock.picking.type'].sudo().search(
+            [('default_location_src_id', '=', self.src_location_id.id), ('code', '=', 'internal')], limit=1)
+        obj.sudo().create({
+            'picking_type_id': obj_picking.id,
+            'location_id': self.src_location_id.id,
+            'location_dest_id': location_id.id,
+            'move_ids_without_package': list_products
+        })
