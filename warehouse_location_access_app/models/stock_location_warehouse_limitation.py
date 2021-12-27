04ced01294ddf14a0d9fa05525bb479bfd2cfa21
amr.hesham <amr.hesham@odootec.com>
2021-12-27 20:33:44 +0400

    edit

--- warehouse_location_access_app/models/stock_location_warehouse_limitation.py
+++ warehouse_location_access_app/models/stock_location_warehouse_limitation.py
@@ -89,8 +89,8 @@ class Warehouse(models.TransientModel):
                     'name': _('Transfer'),
                     'res_model': 'stock.picking',
                     'target': 'current',
-                    'view_id': self.env.ref('stock.view_picking_form').id,
-                    'view_mode': 'form',
-                    'res_id': obj.id,
+                    'view_id': False,
+                    'view_mode': 'tree,form',
+                    # 'res_id': obj.id,
                     'domain' : [('id','=',obj.id)]
                     }
