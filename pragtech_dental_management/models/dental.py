58d410aa08524c56f3257d78897bb9668449341d
mohamedhamdy123890456@gmail.com <mohamedhamdy123890456@gmail.com>
2022-03-12 01:14:27 +0200

    /

--- pragtech_dental_management/models/dental.py
+++ pragtech_dental_management/models/dental.py
@@ -1680,7 +1680,7 @@ class MedicalAppointment(models.Model):
     unit_id = fields.Many2one(comodel_name="medical.hospital.unit", string="",
                               required=False, )
     branch_id = fields.Many2one(
-        'dental.branch', group_expand='_group_expand_branch', related="room_id.branch_id"
+        'dental.branch', group_expand='_group_expand_branch', related="room_id.branch_id",store=True
     )
     is_doctor = fields.Boolean(compute="check_is_doctor")
 
