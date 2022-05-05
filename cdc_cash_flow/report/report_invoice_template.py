243c2d909c2feb14f6c739d2bc272f29d178ede5
mohamedhamdy1233890456@gmail.com <mohamedhamdy1233890456@gmail.com>
2022-05-04 17:56:51 -0700

    /

--- cdc_cash_flow/report/report_invoice_template.py
+++ cdc_cash_flow/report/report_invoice_template.py
@@ -9,17 +9,17 @@ class ReportXlsx(models.AbstractModel):
     _inherit = 'report.report_xlsx.abstract'
     _description = 'Report Xlsx'
 
-    def write_tags(self,tags,row,sheet,format_white,rec):
+    def write_tags(self, tags, row, sheet, format_white, rec,format_invoice,format_tage):
         cr = self._cr
+        total_tags = 0
         for tag in tags:
             total_tag = 0
             row += 1
-            sheet.write(row, 1, tag.name, format_white)
+            sheet.write(row, 1, tag.name, format_tage)
             accounts = self.env['account.account'].sudo().search([('tage_id', '=', tag.id)])
             for account in accounts:
                 row += 1
                 sheet.write(row, 2, account.display_name, format_white)
-
                 cr.execute("""  
                                         SELECT debit,credit FROM account_move_line
                                         WHERE account_id = %s
@@ -31,11 +31,18 @@ class ReportXlsx(models.AbstractModel):
                 credit = sum([x[-1] for x in data])
                 sheet.write(row, 3, str(round(debit, 2)), format_white)
                 total_tag += round(debit, 2)
-        return row,total_tag,tag
+            row+=1
+            sheet.write(row, 1, 'Total ' + str(tag.name), format_invoice)
+            sheet.write(row, 3, str(round(total_tag,2)), format_white)
+
+            total_tags+=total_tag
+        return row, round(total_tags,2), tag
 
     def generate_xlsx_report(self, workbook, data, records):
 
         sheet = workbook.add_worksheet('Invoices Report')
+        format_tage = workbook.add_format(
+            {'font_size': 15, 'border': True, 'align': 'center', 'bg_color': '#33FF9F', 'color': 'black', })
         format_customer = workbook.add_format(
             {'font_size': 15, 'border': True, 'align': 'center', 'bg_color': '#333f4f', 'color': '#ffffff', })
         format_invoice = workbook.add_format(
@@ -55,20 +62,20 @@ class ReportXlsx(models.AbstractModel):
         for rec in records:
             sheet.write(0, 0, 'FROM : ' + str(rec.date_from), format_white)
             sheet.write(0, 2, 'TO : ' + str(rec.date_to), format_white)
-            sheet.write(1, 0, 'CDC CASh FLOW', format_white)
+            sheet.write(1, 0, 'CDC Cash FLOW', format_white)
 
             tags = set(self.env['account.tage'].sudo().search([('type', '=', 'in')]))
             row = 3
-            sheet.write(row, 1, 'CASh IN', format_customer)
-            row,total_tag,tag=self.write_tags(tags,row,sheet,format_white,rec)
+            sheet.write(row, 1, 'Cash IN', format_customer)
+            row, total_tags, tag = self.write_tags(tags, row, sheet, format_white, rec,format_invoice,format_tage)
             row += 1
-            sheet.write(row, 1, 'Total ' + str(tag.name), format_invoice)
-            sheet.write(row, 3, str(total_tag), format_invoice)
-            row+=1
+            sheet.write(row, 1, 'Total Cash IN', format_tage)
+            sheet.write(row, 3, str(total_tags), format_tage)
+            row += 1
 
             tags = set(self.env['account.tage'].sudo().search([('type', '=', 'out')]))
-            sheet.write(row, 1, 'CASh OUT', format_customer)
-            row, total_tag, tag = self.write_tags(tags, row, sheet, format_white, rec)
+            sheet.write(row, 1, 'Cash OUT', format_customer)
+            row, total_tags, tag = self.write_tags(tags, row, sheet, format_white, rec,format_invoice,format_tage)
             row += 1
-            sheet.write(row, 1, 'Total ' + str(tag.name), format_invoice)
-            sheet.write(row, 3, str(total_tag), format_invoice)
+            sheet.write(row, 1, 'Total Cash OUT' , format_tage)
+            sheet.write(row, 3, str(total_tags), format_tage)
